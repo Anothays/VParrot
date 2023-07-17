946f25eb4e6920527d178fc30c1f95b8211c5d08
Jeremy Sananikone <jeremy.snnk@gmail.com>
2023-07-17 14:33:43 +0200

    ajout de la pagination en ajax

--- assets/carsPage.js
+++ assets/carsPage.js
@@ -1,6 +1,11 @@
 import * as bootstrap from "bootstrap";
 
-window.onload = () => {
+window.addEventListener('DOMContentLoaded', () => {
+    initPage()
+})
+
+// init AJAX filters
+function initFilters() {
     const filtersInput = document.querySelectorAll("#filters-form input")
     filtersInput.forEach(input => {
         let initialValue = input.value
@@ -10,10 +15,11 @@ window.onload = () => {
         })
         input.addEventListener('change', (e) => {
             e.preventDefault()
+            console.log(initialValue)
             const [idField,idSuffix] = e.target.id.split('-')
             let oppositeInput = null
             let oppositeInputValue = null
-            switch (idSuffix) {
+            switch (idSuffix) { // On empêche le max d'être inférieur au min et vice-versa
                 case 'max':
                     oppositeInput = document.getElementById(`${idField}-min`)
                     oppositeInputValue = parseInt(oppositeInput.value)
@@ -31,19 +37,18 @@ window.onload = () => {
                 default:
                     break
             }
-            if (e.target.value !== initialValue) {
-                // on récupère l'élément html form
-                const filtersForm = document.getElementById('filters-form')
-                const form = new FormData(filtersForm)
-                const params = new URLSearchParams()
-                params.append('ajax', 'filters')
-                const url = new URL(window.location.href)
-                form.forEach((val, key) => params.append(key,val))
-                fetch(`${url}?${params.toString()}`, {
-                    headers: {
-                        content: 'application/json'
-                    }
-                })
+            // on récupère l'élément html form
+            const filtersForm = document.getElementById('filters-form')
+            const form = new FormData(filtersForm)
+            const params = new URLSearchParams()
+            params.append('ajax', 'filters')
+            const url = new URL(window.location.href)
+            form.forEach((val, key) => params.append(key,val))
+            fetch(`${url}?${params.toString()}`, {
+                headers: {
+                    content: 'application/json'
+                }
+            })
                 .then(res => res.json())
                 .then(data => {
                     // console.log(data)
@@ -52,26 +57,52 @@ window.onload = () => {
                         carsListItems.innerHTML = "<div class='alert alert-info'><p class='text-info text-xl-center'>Aucun véhicule ne correspond à vos critères de recherche !</p></div>"
                     } else {
                         carsListItems.innerHTML = data.content.content
+                        // On pré-remplie le formulaire avec la référence de l'annonce
+                        preFillForm()
                     }
                 })
                 .catch(error => {
                     console.log(error)
                 })
-            }
         })
     })
+}
 
-    // Notification Toast
+// On pré-remplie le formulaire avec la référence de l'annonce
+function preFillForm() {
+    const buttons = document.querySelectorAll('.trigerFormModal')
+    buttons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const id = button.id.slice(4)
+            const ref = +document.getElementById(`ref-${id}`).innerText.slice(5)
+            const objectForm = document.getElementById('contact_subject')
+            const brand = document.getElementById(`brand-${id}`).innerText
+            const model = document.getElementById(`model-${id}`).innerText
+            const modalTitle = document.getElementById('staticBackdropLabel')
+            objectForm.value = `ref : ${ref} - ${brand} ${model}`
+            modalTitle.innerText = `ref : ${ref}`
+        })
+    })
+}
+
+// init boostrap item
+function initPage() {
+    initFilters()
+    preFillForm()
+    // notification toast
     const toastLiveExample = document.getElementById('liveToast')
-    const modalForm = document.getElementById('staticBackdrop')
     const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    // form modal
+    const modalForm = document.getElementById('staticBackdrop')
     const modalFormBootstrap = bootstrap.Modal.getOrCreateInstance(modalForm)
-    const modalFormSubmitButton = document.getElementById('modalFormSubmitButton')
+    // bouton submit du formulaire contenu dans la modale
+    const modalFormSubmitButton = document.getElementById('FormSubmitButton')
     modalFormSubmitButton.addEventListener('click', (e) => {
         e.preventDefault()
         toastBootstrap.show()
         const url = new URL(window.location.href)
         const params = new URLSearchParams()
+        params.append('ajax', 'formSubmit')
         const data = document.getElementById('contactForm')
         const form = new FormData(data)
         form.forEach((val, key) => params.append(key, val))
@@ -81,10 +112,10 @@ window.onload = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                modalFormBootstrap.hide()
+                if (modalFormBootstrap) {
+                    modalFormBootstrap.hide()
+                }
             })
             .catch(error => console.log(error))
     })
 }
-
-
